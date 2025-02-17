23f157a0c70c090e80eb8f910f044a08d6d0ee1a
marta1873 <145761091+marta1873@users.noreply.github.com>
2025-02-17 22:00:12 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import argparse
 import torch
 import numpy as np
+import json
 from text_to_image_model import Diffusion
 from dataset import ShapesDataset
 from experiments import run_experiments, sample_prompts
-from experiment_1 import config_dict
-from experiment_2 import config_dict_2_1
-from experiment_3 import (config_dict_3_0, config_dict_3_1, config_dict_3_2, config_dict_3_3, config_dict_3_4,
-                          config_dict_3_5, config_dict_3_6)
-from experiment_4 import config_dict_4_1
 from evaluate_images import evaluate_accuracy_batch, load_classifiers
 
 
@@ -22,6 +18,10 @@ def main(args):
     # Diffusion model
     model = Diffusion(args)
 
+    # Validate train_dir based on experiment number
+    if args.experiment_num in [1, 3] and args.train_dir != 'complete_dataset':
+        raise ValueError("For experiments 1 and 3, args.train_dir must be 'complete_dataset'.")
+
     if args.task == 'train':
         # Train and test datasets
         train_set = ShapesDataset(db_path=[f'data/{args.train_dir}'])
@@ -32,100 +32,72 @@ def main(args):
     elif args.task == 'sample':
         # Load checkpoint
         model.load_checkpoint(args.ckpt_sample)
-        # Sample using prompt
-        prompt = args.prompt
-        imgs = model.sample(1, prompt, return_img=True)
 
-        # Load pre-trained classifiers for evaluation
-        model_list = load_classifiers()
-        for classifier in model_list:
-            classifier.eval()
-        # Use classifiers to evaluate accuracy
-        evaluate_accuracy_batch(imgs, model_list, ['colour', 's1', 's2'], prompt,
-                                save_directory=f'{model.local_dir}/samples_{prompt}')
+        # Sample input prompt
+        if args.prompt:
+            # Sample using prompt
+            prompt = args.prompt
+            imgs = model.sample(100, prompt, return_img=True)
 
-    elif args.task == 'experiment':
-        # Baseline
-        if args.experiment_num == 1:
-            # Train model
-            train_set = ShapesDataset(db_path=[f'data/complete_dataset'])
-            test_set = ShapesDataset(db_path=['data/test_set'])
-            model.train_diffusion(train_set, test_set)
-            model.load_checkpoint(args.ckpt_sample)
+            # Load pre-trained classifiers for evaluation
+            model_list = load_classifiers()
+            for classifier in model_list:
+                classifier.eval()
+            # Use classifiers to evaluate accuracy
+            evaluate_accuracy_batch(imgs, model_list, ['colour', 's1', 's2'], prompt,
+                                    save_directory=f'{model.local_dir}/samples_{prompt}')
 
-            # Implement steering using prompts in config_dict
-            for test in range(24):
-                run_experiments(model, test, config_dict, space=args.space)
+        # If implementing experiments 1-4, sample the target concepts in a configuration dict
+        else:
+            # Load config_dict
+            with open(args.config_dict_path, 'r') as f:
+                config_dict = json.load(f)
+
+            # Adjust parameters for experiment 3
+            if args.experiment_num == 3:
+                model.p_vec = torch.zeros(1, 10, 512, device=model.device, requires_grad=False, dtype=torch.float32)
+                config_dict = config_dict[args.test]
+
+            # Convert config_dict keys to int
+            config_dict = {int(k): v for k, v in config_dict.items()}
 
             # Sample target concepts in config_dict
             sample_prompts(model, config_dict)
 
-        # Scarcity
-        elif args.experiment_num == 2:
-            # Train model
-            train_set = ShapesDataset(db_path=[f'data/{args.train_dir}'])
-            test_set = ShapesDataset(db_path=['data/test_set'])
-            model.train_diffusion(train_set, test_set)
-            model.load_checkpoint(args.ckpt_sample)
+    elif args.task == 'steer':
+        # Load checkpoint
+        model.load_checkpoint(args.ckpt_sample)
 
-            # Implement steering using prompts in config_dict
-            for test in range(17):
-                run_experiments(model, test, config_dict_2_1, space=args.space)
+        # Load config_dict
+        with open(args.config_dict_path, 'r') as f:
+            config_dict = json.load(f)
 
-            # Sample target concepts in config_dict
-            sample_prompts(model, config_dict_2_1)
+        # Select config dict for experiment 3
+        if args.experiment_num == 3:
+            config_dict = config_dict[args.test]
 
-        # Underspecification
-        elif args.experiment_num == 3:
-            # Train model
-            train_set = ShapesDataset(db_path=[f'data/complete_dataset'])
-            test_set = ShapesDataset(db_path=['data/test_set'])
-            model.train_diffusion(train_set, test_set)
-            model.load_checkpoint(args.ckpt_sample)
+        # Convert config_dict keys to int
+        config_dict = {int(k): v for k, v in config_dict.items()}
 
-            # Define config_dict according to test (level of specification explored)
-            config_dicts_3 = [config_dict_3_0, config_dict_3_1, config_dict_3_2, config_dict_3_3, config_dict_3_4,
-                              config_dict_3_5, config_dict_3_6]
-            config_dict_3 = config_dicts_3[args.test]
-
-            # Implement steering using prompts in config_dict
-            for test in range(10):
-                run_experiments(model, test, config_dict_3, space=args.space)
-
-            # Sample target concepts in config_dict
-            model.p_vec = torch.zeros(1, 10, 512, device=model.device, requires_grad=False, dtype=torch.float32)
-            sample_prompts(model, config_dict_3)
-
-        # Biases
-        elif args.experiment_num == 4:
-            # Train model
-            train_set = ShapesDataset(db_path=[f'data/{args.train_dir}'])
-            test_set = ShapesDataset(db_path=['data/test_set'])
-            model.train_diffusion(train_set, test_set)
-            model.load_checkpoint(args.ckpt_sample)
-
-            # Implement steering using prompts in config_dict
-            for test in range(18):
-                run_experiments(model, test, config_dict_4_1, space=args.space)
-
-            # Sample target concepts in config_dict
-            sample_prompts(model, config_dict_4_1)
+        # Implement steering
+        for test in range(len(config_dict)):
+            run_experiments(model, test, config_dict, space=args.space)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--task', default='experiment', type=str, choices=['train', 'sample',
-                        'experiment'], help='task to implement')
+    parser.add_argument('--task', default='train', type=str, choices=['train', 'sample', 'steer'],
+                        help='task to implement')
     parser.add_argument('--experiment_num', default=1, type=int, help='experiment number in results')
     parser.add_argument('--test', default=0, type=int, help='test index for experiment 3')
-    parser.add_argument('--space', default='h', type=str, choices=['h', 'p'],
+    parser.add_argument('--space', default='p', type=str, choices=['p', 'h'],
                         help='space where steering is implemented')
-
-    parser.add_argument('--train_dir', default='data/complete_dataset', type=str, help='train set directory')
-    parser.add_argument('--prompt', default='a blue circle behind a red triangle', type=str,
+    parser.add_argument('--config_dict_path', default='', type=str,
+                        help='path from which to load dictionary containing target and starting prompts')
+    parser.add_argument('--train_dir', default='complete_dataset', type=str,
+                        help='path to train set directory (within data directory)')
+    parser.add_argument('--prompt', default=None, type=str,
                         help='prompt for sampling (for sample task)')
-
     parser.add_argument('--T', default=1000, type=int, help='number of noising steps')
     parser.add_argument('--seed', default=0, type=int, help='random seed')
     parser.add_argument('--batch_size', default=128, type=int, help='batch size for training model')
@@ -136,5 +108,4 @@ if __name__ == '__main__':
     parser.add_argument('--num_workers', default=2, type=int, help='dataloader num_workers')
 
     args = parser.parse_args()
-
     main(args)
